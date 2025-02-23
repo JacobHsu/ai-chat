bbf00e5209b41e7893c9d5ff42d0bfcc7f62ad7e
jacobhsu <jacob.hsu.tw@gmail.com>
2025-02-23 18:43:05 +0800

    refactor: setting footer

--- src/features/Setting/Footer.tsx
+++ src/features/Setting/Footer.tsx
@@ -1,18 +1,10 @@
 'use client';
 
-import { Icon } from '@lobehub/ui';
 import { createStyles } from 'antd-style';
-import { MessageSquareHeart } from 'lucide-react';
-import Link from 'next/link';
-import { PropsWithChildren, memo, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { PropsWithChildren, memo } from 'react';
 import { Center, Flexbox } from 'react-layout-kit';
 
-import GuideModal from '@/components/GuideModal';
-import GuideVideo from '@/components/GuideVideo';
-import { GITHUB } from '@/const/url';
 import { featureFlagsSelectors, useServerConfigStore } from '@/store/serverConfig';
-import { isOnServerSide } from '@/utils/env';
 
 const useStyles = createStyles(
   ({ css, token }) => css`
@@ -22,52 +14,16 @@ const useStyles = createStyles(
 );
 
 const Footer = memo<PropsWithChildren>(() => {
-  const { t } = useTranslation('common');
-  const [openStar, setOpenStar] = useState(false);
   const { styles } = useStyles();
 
   const { hideGitHub } = useServerConfigStore(featureFlagsSelectors);
 
   return hideGitHub ? null : (
-    <>
-      <Flexbox justify={'flex-end'}>
-        <Center
-          as={'footer'}
-          className={styles}
-          flex={'none'}
-          horizontal
-          padding={16}
-          width={'100%'}
-        >
-          <div style={{ textAlign: 'center' }}>
-            <Icon icon={MessageSquareHeart} /> {`${t('footer.title')} `}
-            <Link
-              aria-label={'star'}
-              href={GITHUB}
-              onClick={(e) => {
-                e.preventDefault();
-                setOpenStar(true);
-              }}
-            >
-              {t('footer.action.star')}
-            </Link>
-          </div>
-        </Center>
-      </Flexbox>
-      <GuideModal
-        cancelText={t('footer.later')}
-        cover={<GuideVideo height={269} src={'/videos/star.mp4?v=1'} width={358} />}
-        desc={t('footer.star.desc')}
-        okText={t('footer.star.action')}
-        onCancel={() => setOpenStar(false)}
-        onOk={() => {
-          if (isOnServerSide) return;
-          window.open(GITHUB, '__blank');
-        }}
-        open={openStar}
-        title={t('footer.star.title')}
-      />
-    </>
+    <Flexbox justify={'flex-end'}>
+      <Center as={'footer'} className={styles} flex={'none'} horizontal padding={16} width={'100%'}>
+        {}
+      </Center>
+    </Flexbox>
   );
 });
 
